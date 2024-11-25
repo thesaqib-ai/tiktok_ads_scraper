c3e4a5d29138ad1cb85a7d94e33a01648e024c21
thesaqib-ai <md.saqib370@gmail.com>
2024-11-25 18:50:52 +0500

    Update tiktok_ads_scraper.py

--- tiktok_ads_scraper.py
+++ tiktok_ads_scraper.py
@@ -189,7 +189,7 @@ def getTikTokAds():
                                 "country": "US",
                                 "order_by": "ctr",
                                 "like": "1",
-                                "ad_format": "1",
+                                "ad_format": "2",
                                 "industry": industry_id,
                                 "ad_language": "en"
                             }
