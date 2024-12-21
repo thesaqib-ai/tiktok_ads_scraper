02f2450bdaed7b13894109f694cac85632293f88
thesaqib-ai <md.saqib370@gmail.com>
2024-12-21 10:33:44 +0500

    Update tiktok_ads_scraper.py

--- tiktok_ads_scraper.py
+++ tiktok_ads_scraper.py
@@ -285,7 +285,7 @@ def getTikTokAds():
     selected_countries_query = ",".join(selected_countries)
     
     # Multiselect for categories
-    selected_categories = st.multiselect("Select Categories:", category_names, default=["Bags"]) 
+    selected_categories = st.multiselect("Select Categories:", category_names) 
     selected_categories_ids = [category_ids[category_names.index(category)] for category in selected_categories]
 
     
